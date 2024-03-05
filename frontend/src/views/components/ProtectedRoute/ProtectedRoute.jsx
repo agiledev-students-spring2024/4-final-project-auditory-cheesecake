d57dfa81e254aa7f96bc5ca596edfe5800ea55b3
j4mesli <jamesli196q@gmail.com>
2024-03-05 18:56:39 -0500

    misc. changes

--- frontend/src/views/components/ProtectedRoute/ProtectedRoute.jsx
+++ frontend/src/views/components/ProtectedRoute/ProtectedRoute.jsx
@@ -3,13 +3,15 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import './ProtectedRoute.css';
 
-const checkAuth = () => {
+const checkAuth = (children) => {
   //   sample function, just return true always for now
+  const desiredPage = children.type.name;
+  console.log(desiredPage);
   return true;
 };
 
 const ProtectedRoute = ({ children }) => {
-  const isAuthenticated = checkAuth();
+  const isAuthenticated = checkAuth(children);
   return isAuthenticated ? children : <Navigate to="/" />;
 };
 
