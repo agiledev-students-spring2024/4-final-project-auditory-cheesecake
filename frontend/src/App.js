6a6dc31886586f74e98a2316f4e77bbf1d117f23
samchensd <92701644+samchensd@users.noreply.github.com>
2024-03-05 18:33:13 -0500

    Added profile section (#39)
    * addded barebones profile page
    
    * changed to jsx from js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './views/HomePage/HomePage';
+import Profile from './views/Profile/Profile';
 import Results from './views/Results/Results';
 import NavBar from './components/NavBar/NavBar';
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
@@ -22,7 +23,7 @@ function App() {
 
           
           {/* Route for Profile Page */}
-          {/* <Route path="/contact" element={<Profile />} /> */}
+          <Route path="/profile" element={<Profile />} />
           
           {/* Route for Results Page */}
           {/* !!!!! GUARD THIS ROUTE SO THAT IT CAN ONLY BE ACCESSED W/ CERTAIN CONDITIONS !!!!! */}
