bfd531b01ba4c492548e09d409cce12f0fad3c17
suhamemon1 <155314925+suhamemon1@users.noreply.github.com>
2024-03-13 15:32:04 -0400

    fixed most of the style and added hamburger menu

--- frontend/src/views/components/NavBar/NavBar.jsx
+++ frontend/src/views/components/NavBar/NavBar.jsx
@@ -1,26 +1,40 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import './NavBar.css';
 import logo from '../../../fullLogo.png'; // Adjust the path as necessary
+import { slide as Menu } from 'react-burger-menu';
+
 
 const NavBar = () => {
-    const navigate = useNavigate();
-
     return (
         <nav className="NavBar">
             <ul>
                 {/* change as needed (we need the actual links below, nothing out of the ordinary) */}
-                <div className="logo-container">
-                    <img src={logo} alt="Logo" style={{ height: '50px' }} /> {/* Adjust styling as needed */}
-                </div>
-                <li><button onClick={() => navigate("/")}>Home</button></li>
-                <li><button onClick={() => navigate("/Survey")}>Quiz</button></li>
-                <li><button onClick={() => navigate("/Profile")}>Profile</button></li>
-                <li><button onClick={() => navigate("/Login")}>
-                    <span className="material-symbols-outlined">
-                        login
-                    </span>
-                </button></li>
+                <li>
+                    <div className="logo-container">
+                        <img class='logo' src={logo} alt="Logo"/> {/* Adjust styling as needed */}
+                    </div>
+                </li>
+                
+                <li>
+                    <Menu right>
+                    <a className="menu-item" href="/">
+                        Home
+                    </a>
+                    <a className="menu-item" href="/Survey">
+                        Quiz
+                    </a>
+                    <a className="menu-item" href="/Profile">
+                        Profile
+                    </a>
+                    <a className="menu-item" href="/Login">
+                        Login
+                        <span className="material-symbols-outlined">
+                            login
+                        </span>   
+                    </a>
+                    </Menu>
+                </li>
             </ul>
         </nav>
     );
