6243160de795cd8868da27b36df2868dabbc6a4a
fctico11 <fac322@nyu.edu>
2024-03-26 15:20:56 -0400

    User registration for mock data

--- back-end/app.js
+++ back-end/app.js
@@ -1,17 +1,20 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); 
-app.use(bodyParser.json());
 app.use(cors()); 
 
 const surveyRoutes = require('./routes/survey');
 app.use('/api', surveyRoutes);
+
+const authRoutes = require('./routes/auth');
+app.use('/api/auth', authRoutes);
+
 
 //connect to MongoDB
 //mongoose.connect('url',;
