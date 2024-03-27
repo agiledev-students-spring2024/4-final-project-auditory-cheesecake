6b7f306db5d5ad564d3a62dd16ac0a1c3dd9956b
samchensd <samchensd@gmail.com>
2024-03-27 19:50:22 -0400

    merging with main

---
+++ back-end/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); 
+app.use(bodyParser.json());
+app.use(cors()); 
+
+const surveyRoutes = require('./routes/survey');
+app.use('/api', surveyRoutes);
+
+//connect to MongoDB
+//mongoose.connect('url',;
+
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app; 
