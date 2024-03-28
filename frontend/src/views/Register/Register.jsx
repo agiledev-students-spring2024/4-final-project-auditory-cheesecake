a43c94499980602a822a1b346f41c157e8a58ac1
fctico11 <fac322@nyu.edu>
2024-03-27 23:06:08 -0400

    changed port number in frontend

--- frontend/src/views/Register/Register.jsx
+++ frontend/src/views/Register/Register.jsx
@@ -53,7 +53,7 @@ const Register = () => {
         //console.log(email, username, password);
 
     //API call to the backend registration endpoint
-    fetch('http://localhost:5000/api/auth/register', {
+    fetch('http://localhost:1337/api/auth/register', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
