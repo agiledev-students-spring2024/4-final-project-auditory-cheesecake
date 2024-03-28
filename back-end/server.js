e4e43bdc16b0bf2364d3f426dfeacaec31c7a164
Ibrahimsheikh02 <mis9351@nyu.edu>
2024-03-28 06:43:05 -0400

    added tests

---
+++ back-end/server.js
@@ -0,0 +1,7 @@
+// Change from CommonJS to ES Modules syntax
+import app from './app.js'; 
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
