40cd242c4a84e62aba08076ee48599c7d502dfe9
samchensd <samchensd@gmail.com>
2024-04-09 13:02:09 -0400

    set up mongodb

--- back-end/server.js
+++ back-end/server.js
@@ -2,6 +2,10 @@
 
 const app = require('./app.js');
 const PORT = process.env.PORT || 1337;
+
+const connectDatabase = require('./database');
+
+connectDatabase();
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
