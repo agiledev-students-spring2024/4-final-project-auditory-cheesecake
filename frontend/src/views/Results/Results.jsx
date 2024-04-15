8068cc032893bcd66f0abb5db7dcc6203becc20d
suhamemon1 <155314925+suhamemon1@users.noreply.github.com>
2024-04-14 22:11:40 -0400

    Collaboratively working on results

--- frontend/src/views/Results/Results.jsx
+++ frontend/src/views/Results/Results.jsx
@@ -50,13 +50,13 @@ const ProgressBar = ({ trait, score }) => (
   </div>
 );
 
-const ProgressCircle = ({ score }) => (
-  <div className="progress-circle-container"> {/* Additional class for specificity */}
-    <div className="progress-circle-bar">
-      <div className="progress" style={{ width: `${score}%` }}>{score}%</div>
-    </div>
-  </div>
-);
+// const ProgressCircle = ({ score }) => (
+//   <div className="progress-circle-container"> {/* Additional class for specificity */}
+//     <div className="progress-circle-bar">
+//       <div className="progress" style={{ width: `${score}%` }}>{score}%</div>
+//     </div>
+//   </div>
+// );
 
 const Results = () => {
   const [responses, setResponses] = useState([]);
@@ -94,7 +94,7 @@ const Results = () => {
           <ProgressBar key={trait} trait={trait} score={score} />
         ))}
         <div className="results-description">
-          <p>Each progress bar represents your score in one of the Big Five personality traits: Openness, Conscientiousness, Extraversion, Agreeableness, and Neuroticism. A higher score indicates a stronger presence of that trait in your personality.</p>
+          <p>Each progress bar represents your score in one of the Big Five personality traits: Openness, Conscientiousness, Extraversion, Agreeableness, and Neuroticism. A higher score indicates a stronger presence of that trait in your personality.  Keep reading below to learn more about your personality.</p>
         </div>
         {loading ? <p>Loading responses...</p> : (
           <div>
@@ -109,6 +109,59 @@ const Results = () => {
             ))}
           </div>
         )}
+        {/* <div className="all-traits">
+          <div className="individual-trait">
+            <h3 className="trait-title">Openness</h3>
+            <div className="ocean-img">
+              <img src="/Openness.png" alt="openness"/>
+            </div>
+            <ProgressCircle score={bigFiveScores.Openness} />
+            <div className="trait-desc">
+              Openness (also referred to as openness to experience) emphasizes imagination and insight the most out of all five personality traits. People who are high in openness tend to have a broad range of interests. They are curious about the world and other people and are eager to learn new things and enjoy new experiences. People who are high in this personality trait also tend to be more adventurous and creative. Conversely, people low in this personality trait are often much more traditional and may struggle with abstract thinking.
+            </div>
+          </div>
+          <div className="individual-trait">
+            <h3 className="trait-title">Conscientiousness</h3>
+            <div className="ocean-img">
+              <img src="/Conscientiousness.png" alt="conscientiousness"/>
+            </div>
+            <ProgressCircle score={bigFiveScores.Conscientiousness} />
+            <div className="trait-desc">
+              Conscientiousness is one defined by high levels of thoughtfulness, good impulse control, and goal-directed behaviors. Highly conscientious people tend to be organized and mindful of details. They plan ahead, think about how their behavior affects others, and are mindful of deadlines. Someone scoring lower in this primary personality trait is less structured and less organized. They may procrastinate to get things done, sometimes missing deadlines completely.
+            </div>
+          </div>
+          <div className="individual-trait">
+            <h3 className="trait-title">Extraversion</h3>
+            <div className="ocean-img">
+              <img src="/Extraversion.png" alt="extraversion"/>
+            </div>
+            <ProgressCircle score={bigFiveScores.Extraversion} />
+            <div className="trait-desc">
+              Extraversion (or extroversion) is a personality trait characterized by excitability, sociability, talkativeness, assertiveness, and high amounts of emotional expressiveness. People high in extraversion are outgoing and tend to gain energy in social situations. Being around others helps them feel energized and excited. People who are low in this personality trait or introverted tend to be more reserved. They have less energy to expend in social settings and social events can feel draining. Introverts often require a period of solitude and quiet in order to "recharge."
+            </div>
+          </div>
+          <div className="individual-trait">
+            <h3 className="trait-title">Agreeableness</h3>
+            <div className="ocean-img">
+              <img src="/Agreeableness.png" alt="agreeableness"/>
+            </div>
+            <ProgressCircle score={bigFiveScores.Agreeableness} />
+            <div className="trait-desc">
+              This personality trait includes attributes such as trust, altruism, kindness, affection, and other prosocial behaviors. People who are high in agreeableness tend to be more cooperative while those low in this personality trait tend to be more competitive and sometimes even manipulative.
+            </div>
+          </div>
+          <div className="individual-trait">
+            <h3 className="trait-title">Neuroticism</h3>
+            <div className="ocean-img">
+              <img src="/Neuroticism.png" alt="neuroticism"/>
+            </div>
+            <ProgressCircle score={bigFiveScores.Neuroticism} />
+            <div className="trait-desc">
+              Neuroticism is a personality trait characterized by sadness, moodiness, and emotional instability. Individuals who are high in neuroticism tend to experience mood swings, anxiety, irritability, and sadness. Those low in this personality trait tend to be more stable and emotionally resilient.
+            </div>
+          </div>
+
+        </div> */}
       </div>
     </div>
   );
