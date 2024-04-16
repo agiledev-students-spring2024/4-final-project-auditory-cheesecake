2348b856a7753f984583ef913fc513c7b081b37d
suhamemon1 <155314925+suhamemon1@users.noreply.github.com>
2024-04-16 14:00:50 -0400

    mostly done with edit profile

--- frontend/src/views/EditProfile/EditProfile.jsx
+++ frontend/src/views/EditProfile/EditProfile.jsx
@@ -1,19 +1,92 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import './EditProfile.css';
+import axios from 'axios';
+
 
 const EditProfile = () => {
-    const [email, setEmail] = useState('first.last@email.com');
-    const [username, setUsername] = useState('myUserName');
-    const [firstName, setFirstName] = useState('First');
-    const [lastName, setLastName] = useState('Last');
-    const [phoneNumber, setPhoneNumber] = useState('1234567890');
+    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [emailValid, setEmailValid] = useState(true); // State to track if the email is valid
+    const [firstNameValid, setFirstNameValid] = useState(true);
+    const [lastNameValid, setLastNameValid] = useState(true);
+    const [usernameValid, setUsernameValid] = useState(true);
+    const [phoneNumberValid, setPhoneNumberValid] = useState(true);
+
 
     const navigate = useNavigate();
 
+    useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                const user = JSON.parse(sessionStorage.getItem('user'));
+                console.log('User:', user)
+                const userId = user.id;
+                if (userId) {
+                    const response = await axios.get(`http://localhost:1337/api/user/${userId}`);
+                    const userData = response.data;
+                    console.log('userData: ', userData);
+                    setFirstName(userData.firstName);
+                    setLastName(userData.lastName);
+                    setUsername(userData.username);
+                    setEmail(userData.email);
+                    setPhoneNumber(userData.phoneNumber);
+                }
+            } catch (error) {
+                console.error('Failed to fetch user data:', error);
+            }
+        };
+        fetchUserData();
+    }, []);
+
     const validateEmail = (email) => {
-        return email.search(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/);
-    }
+        const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        return regex.test(email);
+    };
+    const validateFirstName = (firstName) => {
+        return firstName.trim() !== '';
+    };
+    const validateLastName = (lastName) => {
+        return lastName.trim() !== '';
+    };
+    const validateUsername = (username) => {
+        return username.trim() !== '' && username.length >= 4;
+    };
+    const validatePhoneNumber = (phoneNumber) => {
+        const regex = /^(\+1\s?)?(\([0-9]{3}\)|[0-9]{3})[\s.-]?[0-9]{3}[\s.-]?[0-9]{4}$/;
+        return regex.test(phoneNumber);
+    };
+    const handleEmailChange = (e) => {
+        const newEmail = e.target.value.replace(/\s/g, ''); // Remove all spaces
+        setEmail(newEmail);
+        setEmailValid(validateEmail(newEmail));
+    };
+    
+    const handlePhoneNumberChange = (e) => {
+        const onlyDigits = e.target.value.replace(/[^\d]/g, ''); // Allow only digits, no spaces
+        const formattedPhoneNumber = onlyDigits.length <= 10 ? formatPhoneNumber(onlyDigits) : onlyDigits;
+        setPhoneNumber(formattedPhoneNumber);
+        setPhoneNumberValid(validatePhoneNumber(onlyDigits)); // Validate the cleaned number
+    };
+    
+    const handleUsernameChange = (e) => {
+        const newUsername = e.target.value.replace(/\s/g, ''); // Remove all spaces
+        setUsername(newUsername);
+        setUsernameValid(validateUsername(newUsername));
+    };
+
+    const formatPhoneNumber = (value) => {
+        if (!value) return value;
+        const phoneNumber = value.replace(/[^\d]/g, '');
+        const phoneNumberLength = phoneNumber.length;
+        if (phoneNumberLength < 4) return phoneNumber;
+        if (phoneNumberLength < 7) return `${phoneNumber.slice(0, 3)}-${phoneNumber.slice(3)}`;
+        return `${phoneNumber.slice(0, 3)}-${phoneNumber.slice(3, 6)}-${phoneNumber.slice(6, 10)}`;
+    };
+
     // const validateFirstName = (firstName) => {
     //     // Check if firstName is not null, undefined, and not an empty string
     //     return firstName !== null && firstName !== undefined && firstName.trim() !== '';
@@ -26,30 +99,16 @@ const EditProfile = () => {
     //     const regex = /^(\+1\s?)?(\([0-9]{3}\)|[0-9]{3})[\s.-]?[0-9]{3}[\s.-]?[0-9]{4}$/;
     //     return regex.test(phoneNumber);
     // };
+
+
     const handleSubmit = (e) => {
         console.log("Form submit initiated");
         e.preventDefault();
         alert('Edit Profile form is submitting');
-        if (username.length < 4) {
-            alert("Username must be at least 4 characters long!");
+        if (!emailValid || !firstNameValid || !lastNameValid || !usernameValid || !phoneNumberValid) {
+            alert("Please correct the errors in the form.");
             return;
         }
-        else if (validateEmail(email)) {
-            alert("Invalid email!");
-            return;
-        }
-        // else if (validateFirstName(firstName)) {
-        //     alert("Please enter your first name.");
-        //     return;
-        // }
-        // else if (validateLastName(lastName)) {
-        //     alert("Please enter your last name.");
-        //     return;
-        // }
-        // else if (validatePhoneNumber(phoneNumber)) {
-        //     alert("Please enter your phone number.");
-        //     return;
-        // }
         //API call to the backend registration endpoint
         fetch('http://localhost:1337/api/auth/register', {
             method: 'POST',
@@ -85,59 +144,78 @@ const EditProfile = () => {
 
     return (
         <div className="edit-profile">
-        <div className="edit-profile-form-wrapper">
-            <h1>Edit Profile</h1>
-            <form onSubmit={handleSubmit}>
-                
-                <label htmlFor="FirstName">First Name</label>
-                <input 
-                    id="FirstName" 
-                    type="text" 
-                    placeholder="FirstName" 
-                    value={firstName}
-                    onChange={(e) => setFirstName(e.target.value)}
-                    required
-                />
-                <label htmlFor="LastName">Last Name</label>
-                <input 
-                    id="LastName" 
-                    type="text" 
-                    placeholder="LastName" 
-                    value={lastName}
-                    onChange={(e) => setLastName(e.target.value)}
-                    required
-                />
-                <label htmlFor="Username">Username</label>
-                <input 
-                    id="Username" 
-                    type="text" 
-                    placeholder="Username" 
-                    value={username} 
-                    onChange={(e) => setUsername(e.target.value)}
-                    required
-                />
-                <label htmlFor="Email">Email</label>
-                <input 
-                    id="Email" 
-                    type="email" 
-                    placeholder="Email" 
-                    value={email} 
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                />
-                <label htmlFor="PhoneNumber">Phone Number</label>
-                <input 
-                    id="PhoneNumber" 
-                    type="text" 
-                    placeholder="Phone Number" 
-                    value={phoneNumber} 
-                    onChange={(e) => setPhoneNumber(e.target.value)}
-                    required
-                />
-                <button type="submit">Update Profile</button>
-                <p><button onClick={() => navigate("/Profile")} className="button-link">View Profile</button></p>
-            </form>
-        </div>
+            <div className="edit-profile-form-wrapper">
+                <h1>Edit Profile</h1>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="FirstName">First Name</label>
+                    <input
+                        id="FirstName"
+                        type="text"
+                        placeholder="FirstName"
+                        value={firstName}
+                        onChange={(e) => {
+                            setFirstName(e.target.value);
+                            setFirstNameValid(validateFirstName(e.target.value));
+                        }}
+                        required
+                    />
+                    {!firstNameValid && <p className="error-message">Please enter your first name.</p>}
+
+
+                    <label htmlFor="LastName">Last Name</label>
+                    <input
+                        id="LastName"
+                        type="text"
+                        placeholder="LastName"
+                        value={lastName}
+                        onChange={(e) => {
+                            setLastName(e.target.value);
+                            setLastNameValid(validateLastName(e.target.value));
+                        }}
+                        required
+                    />
+                    {!lastNameValid && <p className="error-message">Please enter your last name.</p>}
+
+                    <label htmlFor="Username">Username</label>
+                    <input
+                        id="Username"
+                        type="text"
+                        placeholder="Username"
+                        value={username}
+                        onChange={handleUsernameChange}
+                        required
+                    />
+                    {!usernameValid && <p className="error-message">Username must be at least 4 characters long!</p>}
+
+                    <label htmlFor="Email">Email</label>
+                    <input
+                        id="Email"
+                        type="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={handleEmailChange}
+                        required
+                    />
+                    {!emailValid && email && 
+                        <p className="error-message">Please enter a valid email address.</p>
+                    }
+                    
+                    <label htmlFor="PhoneNumber">Phone Number</label>
+                    <input
+                        id="PhoneNumber"
+                        type="text"
+                        placeholder="Phone Number"
+                        value={phoneNumber}
+                        onChange={handlePhoneNumberChange}
+                        required
+                    />
+                    {!phoneNumberValid && <p className="error-message">Please enter a valid phone number.</p>}
+
+                    <button type="submit" disabled={!emailValid || !firstNameValid || !lastNameValid || !usernameValid || !phoneNumberValid}>Update Profile</button>
+                    {/* <button type="submit" disabled={!emailValid || !email}>Update Profile</button> */}
+                    <p><button onClick={() => navigate("/Profile")} className="button-link">View Profile</button></p>
+                </form>
+            </div>
         </div>
     );
 };
