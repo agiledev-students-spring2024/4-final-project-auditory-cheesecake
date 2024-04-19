42ea0500eeb1dd29e0385b8e3e34abdfdcc8b0c9
j4mesli <jamesli196q@gmail.com>
2024-04-19 18:38:45 -0400

    added cron and semaphore for cache management

--- back-end/server.js
+++ back-end/server.js
@@ -1,15 +1,39 @@
-// Change from CommonJS to ES Modules syntax
-
-const sessionCache = require('./classes/SessionCache.js');
+const cron = require('node-cron');
+const Semaphore = require('semaphore');
 const app = require('./app.js');
+const connectDatabase = require('./database');
+const { sync, flushExpiredSessions } = require('./utility/SessionsUtility');
+
 const PORT = process.env.PORT || 1337;
 
-const connectDatabase = require('./database');
-// Setup cleanup task
-setInterval(() => {
-  sessionCache.clearExpiredSessions();
-}, 3600000);
+// instantiate mutex
+const mutex = Semaphore(1);
 
+// hourly cache cleanup and sync task
+cron.schedule('0 * * * *', () => {
+  // mutex ensures that only one job runs at a time
+  mutex.take(async () => {
+    await flushExpiredSessions();
+    console.log('Hourly session flush executed.');
+    mutex.leave();
+  });
+}, {
+  scheduled: true,
+  timezone: "America/New_York"
+});
+
+// daily synchronization of cache at midnight
+cron.schedule('0 0 * * *', () => {
+  // mutex ensures that only one job runs at a time
+  mutex.take(async () => {
+    await sync();
+    console.log('Daily synchronization complete');
+    mutex.leave();
+  });
+}, {
+  scheduled: true,
+  timezone: "America/New_York"
+});
 
 connectDatabase();
 app.listen(PORT, () => {
