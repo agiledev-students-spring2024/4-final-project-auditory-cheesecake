931d1f13cde60c73310c11c98b8dc81b6a05bdb0
fctico11 <fac322@nyu.edu>
2024-04-23 11:38:48 -0400

    updated terms and conditions

--- frontend/src/views/Profile/Profile.jsx
+++ frontend/src/views/Profile/Profile.jsx
@@ -118,8 +118,28 @@ const Profile = () => {
             <div className="terms-content" onClick={(e) => e.stopPropagation()}>
               <span className="terms-close" onClick={handleTermsToggle}>&times;</span>
               <h2>Terms and Conditions</h2>
-              <p>Here are the terms and conditions... Lorem ipsum dolor sit amet,
-                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...</p>
+              <p>
+Welcome to <strong>Auditory Cheesecake!</strong> These Terms and Conditions outline the rules and regulations for the use of our web-app.
+By accessing this app, we assume you accept these terms and conditions. Do not continue to use Auditory Cheesecake if you do not agree to take all of the terms and conditions stated on this page.</p>
+<p>
+<strong>Account Creation:</strong> Users must be at least 13 years old to create an account on Auditory Cheesecake. By creating an account, you declare you are over the age of 13. Each user must provide a valid email address and accurate information about themselves.
+</p><p><strong>Account Responsibilities:</strong> You are responsible for maintaining the confidentiality of your account and password, including but not limited to the restriction of access to your computer and/or account. You agree to accept responsibility for any and all activities or actions that occur under your account and/or password.
+</p><p><strong>User Content:</strong>
+Surveys and Responses: Users can participate in assesments about their music tastes. The responses to these surveys are saved to the user's profile and can be used by Auditory Cheesecake to personalize recommendations and improve service offerings.
+Ownership and Rights: Users retain ownership of the data they provide but grant Auditory Cheesecake a worldwide, perpetual, irrevocable, royalty-free license to use, store, and reproduce the data for the purposes of providing and improving the app's services.
+</p>
+<p><strong>Prohibited Uses:</strong>
+You may not use our service:
+In any way that breaches any applicable local, national or international law or regulation.
+In any way that is unlawful or fraudulent, or has any unlawful or fraudulent purpose or effect.
+To upload, host, use, publish, share, or otherwise transmit data that contains viruses or other malicious code or harmful components.
+</p>
+<p><strong>Privacy Policy:</strong>
+Please refer to our Privacy Policy for information on how we collect, use, and share your data.
+Disclaimers and Limitations of Liability:
+
+Services Provided "As Is": The services are provided "as is" and "as available" without any warranties, express or implied.
+Limitation of Liability: Auditory Cheesecake will not be liable for any damages or loss resulting from your use of the app</p>
             </div>
           </div>
         )}
