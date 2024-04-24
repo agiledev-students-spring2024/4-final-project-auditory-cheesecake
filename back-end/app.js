7b33a9a439733b3f248d677afdfcdcfcc67d8381
fctico11 <fac322@nyu.edu>
2024-04-23 23:57:06 -0400

    increased image upload limit

--- back-end/app.js
+++ back-end/app.js
@@ -10,7 +10,8 @@ const app = express();
 
 //const PORT = process.env.PORT || 1337;
 
-app.use(express.json()); 
+app.use(express.json({ limit: '10mb' })); 
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cors()); 
 
 app.use(express.static(path.join(__dirname, 'public')));
