593021d8d8696407e6503e601d651008cabbda3d
suhamemon1 <155314925+suhamemon1@users.noreply.github.com>
2024-04-25 11:39:08 -0400

    modified edit user profile tests

--- back-end/test/editUserProfile.test.js
+++ back-end/test/editUserProfile.test.js
@@ -7,67 +7,120 @@ const { expect } = chai;
 chai.use(chaiHttp);
 
 describe('POST /editUserProfile', () => {
-    before(() => {
-      process.env.USE_MOCK_DATA = 'true';
-    });
-  
-    it('should update the user profile data successfully', (done) => {
-      
-      const userId = '11';
-      const username = 'test11';
-      const email = 'test11@gmail.com';
-      const firstName = 'First11';
-      const lastName = 'Last11';
-      const phoneNumber = '1234567891';
-  
-      chai.request(server)
-        .post('/api/editUserProfile')
-        .send({ id: userId, username: username, email: email, firstName:firstName, lastName:lastName, phoneNumber:phoneNumber })
-        .end((err, res) => {
-          expect(res).to.have.status(200);
-          expect(res.body).to.have.property('message').eql('User profile updated successfully in mock data');
-          done();
-        });
-    });
-
-    it('should return an error if any of the fields are missing', (done) => {
-        const userId = '11';
-        const username = 'test11';
-        const email = 'test11@gmail.com';
-        const firstName = '';
-        const lastName = 'Last11';
-        const phoneNumber = '1234567891';
-
-        chai.request(server)
-            .post('/api/editUserProfile')
-            .send({ id: userId, username: username, email: email, firstName: firstName, lastName: lastName, phoneNumber: phoneNumber })
-            .end((err, res) => {
-                expect(res).to.have.status(400);
-                expect(res.body).to.have.property('error').eql('Missing parameters');
-                done();
-            });
-    });
-  
-    it('should return an error if the user is not found', (done) => {
-      const userId = 'nonExistingId';
-      const username = 'test11';
-      const email = 'test11@gmail.com';
-      const firstName = 'First11';
-      const lastName = 'Last11';
-      const phoneNumber = '1234567891';
-  
-      chai.request(server)
-        .post('/api/editUserProfile')
-        .send({ id: userId, username: username, email: email, firstName:firstName, lastName:lastName, phoneNumber:phoneNumber })
-        .end((err, res) => {
-          expect(res).to.have.status(404);
-          expect(res.body).to.have.property('message').eql('User not found in mock data');
-          done();
-        });
-    });
-    
-    
-    after(() => {
-      // Clean up environment
-    });
+  before(() => {
+    process.env.USE_MOCK_DATA = 'true';
   });
+
+  it('should update the user profile data successfully', (done) => {
+
+    const userId = '11';
+    const username = 'TEST11'; // testing case insensitivity
+    const email = 'TEST11@GMAIL.COM'; // testing case insensitivity
+    const firstName = 'First11';
+    const lastName = 'Last11';
+    const phoneNumber = '1234567891';
+
+    chai.request(server)
+      .post('/api/editUserProfile')
+      .send({ id: userId, username, email, firstName, lastName, phoneNumber })
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property('message').eql('User profile updated successfully');
+        done();
+      });
+  });
+
+  it('should return an error if any of the fields are missing', (done) => {
+    const userId = '11';
+    const username = 'test11';
+    const email = 'test11@gmail.com';
+    const firstName = '';
+    const lastName = 'Last11';
+    const phoneNumber = '1234567891';
+
+    chai.request(server)
+      .post('/api/editUserProfile')
+      .send({ id: userId, username, email, firstName, lastName, phoneNumber })
+      .end((err, res) => {
+        expect(res).to.have.status(400);
+        expect(res.body).to.have.property('error').eql('Missing parameters');
+        done();
+      });
+  });
+
+  it('should return an error if the user is not found', (done) => {
+    const userId = 'nonExistingId';
+    const username = 'test11';
+    const email = 'test11@gmail.com';
+    const firstName = 'First11';
+    const lastName = 'Last11';
+    const phoneNumber = '1234567891';
+
+    chai.request(server)
+      .post('/api/editUserProfile')
+      .send({ id: userId, username, email, firstName, lastName, phoneNumber })
+      .end((err, res) => {
+        expect(res).to.have.status(404);
+        expect(res.body).to.have.property('message').eql('User not found');
+        done();
+      });
+  });
+
+  it('should return error if email is already in use by another account', (done) => {
+    const userId = '11';
+    const username = 'test11';
+    const email = 'testuser1712074258415@example.com'; // This email is already in use
+    const firstName = 'First11';
+    const lastName = 'Last11';
+    const phoneNumber = '1234567891';
+
+    chai.request(server)
+      .post('/api/editUserProfile')
+      .send({ id: userId, username, email, firstName, lastName, phoneNumber })
+      .end((err, res) => {
+        expect(res).to.have.status(409);
+        expect(res.body).to.have.property('error').eql('Email already in use by another account.');
+        done();
+      });
+  });
+
+  it('should return error if username is already in use by another account', (done) => {
+    const userId = '11';
+    const username = 'testUser1712074258415'; // this username is already in use
+    const email = 'test11@gmail.com';
+    const firstName = 'First11';
+    const lastName = 'Last11';
+    const phoneNumber = '1234567891';
+
+    chai.request(server)
+      .post('/api/editUserProfile')
+      .send({ id: userId, username, email, firstName, lastName, phoneNumber })
+      .end((err, res) => {
+        expect(res).to.have.status(409);
+        expect(res.body).to.have.property('error').eql('Username already in use by another account.');
+        done();
+      });
+  });
+
+  it('should return error if phone number is already in use by another account', (done) => {
+    const userId = '11';
+    const username = 'test11';
+    const email = 'test11@gmail.com';
+    const firstName = 'First11';
+    const lastName = 'Last11';
+    const phoneNumber = '1234561712074258415'; // this phone number is already in use
+
+    chai.request(server)
+      .post('/api/editUserProfile')
+      .send({ id: userId, username, email, firstName, lastName, phoneNumber })
+      .end((err, res) => {
+        expect(res).to.have.status(409);
+        expect(res.body).to.have.property('error').eql('Phone number already in use by another account.');
+        done();
+      });
+  });
+
+  after(() => {
+    // Clean up environment
+  });
+});
