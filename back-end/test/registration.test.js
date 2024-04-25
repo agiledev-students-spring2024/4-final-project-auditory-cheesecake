b9967da18de771454e5cc0a2a62e563c891f569f
fctico11 <fac322@nyu.edu>
2024-04-25 12:50:07 -0400

    change to register test with real data

--- back-end/test/registration.test.js
+++ back-end/test/registration.test.js
@@ -1,13 +1,23 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const server = require('../app');  
+const mongoose = require('mongoose');
+const server = require('../app'); 
+const connectDB = require('../database.js'); 
 
 const { expect } = chai;
 chai.use(chaiHttp);
 
+before(async () => {
+    await connectDB();
+  });
+  
+after(async () => {
+    await mongoose.disconnect(); 
+  });
+
 describe('User Registration API', () => {
     before(() => {
-        process.env.USE_MOCK_DATA = 'true';  //for testing with mock data
+        process.env.USE_MOCK_DATA = 'false';  //for testing with real data
     });
 
     describe('POST /register', () => {
@@ -27,19 +37,19 @@ describe('User Registration API', () => {
                 .send(newUser)
                 .end((err, res) => {
                     expect(res).to.have.status(201);
-                    expect(res.body).to.have.property('message').eql('User registered successfully in mock data');
+                    expect(res.body).to.have.property('message').eql('User registered successfully');
                     done();
                 });
         });
 
         it('should not register a user with an existing email', (done) => {
             const duplicateUser = {
-                username: 'testUser2',
-                email: 'testuser@example.com',  //using the same email as above
-                password: 'TestPass123!',
-                firstName: 'Test',
-                lastName: 'User2',
-                phoneNumber: '0987654321'
+                username: 'fac333',
+                email: 'test2001@gmail.com',  //using the same email as above
+                password: 'Test#2001',
+                firstName: 'Fran',
+                lastName: 'G',
+                phoneNumber: '9999199999'
             };
 
             chai.request(server)
